5a8cf53be6371301e8a596371679be98cd5a7826
Miguel Alvarez <ing.migueljalvarez@gmail.com>
2024-03-23 13:14:41 -0500

    Add Carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,17 +1,57 @@
 <script>
-import { Hopper, Slide } from "hooper";
+import { defineComponent } from "vue";
+import { Carousel, Pagination, Slide } from "vue3-carousel";
 
-export default {
+import "vue3-carousel/dist/carousel.css";
+
+export default defineComponent({
   name: "CarouselComponent",
   components: {
-    Hopper,
+    Carousel,
     Slide,
+    Pagination,
   },
-};
+  props: {
+    images: { type: Array },
+  },
+  emits: ["play"],
+  mounted() {
+    this.$emit("play");
+  },
+});
 </script>
 <template>
-  <Hopper>
-    <Slide> Slide 1 </Slide>
-    <Slide> Slide 2 </Slide>
-  </Hopper>
+  <Carousel class="h-min" :autoplay="5000" :wrap-around="true">
+    <Slide v-for="image in images" :key="image.id">
+      <div class="carousel__item">
+        <img :src="image.url" :alt="image.name" />
+      </div>
+    </Slide>
+    <template #addons>
+      <Pagination />
+    </template>
+  </Carousel>
 </template>
+<style>
+.carousel__item {
+  min-height: 200px;
+  width: 100%;
+  background-color: var(--vc-clr-primary);
+  color: var(--vc-clr-white);
+  font-size: 20px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.carousel__slide {
+  padding: 10px;
+}
+
+.carousel__prev,
+.carousel__next {
+  box-sizing: content-box;
+  border: 5px solid white;
+}
+</style>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,5 +1,6 @@
 <script>
 import Layout from "@/components/Layout.vue";
+import Carousel from "@/components/Carousel.vue";
 // import image1 from "../../assets/img/banner-main.jpg";
 const banner = require("../../assets/img/banner.jpg");
 const tvDigital = require("../../assets/img/tv-digital.png");
@@ -9,7 +10,8 @@ const alfredoRojas = require("../../assets/img/alfredo-rojas.png");
 import "vue3-carousel/dist/carousel.css";
 export default {
   name: "HomeComponent",
-  components: { Layout },
+  components: { Layout, Carousel },
+
   data: () => {
     return {
       audio: undefined,
@@ -17,23 +19,51 @@ export default {
       tv: tvDigital,
       notiAr,
       alfredoRojas,
+      images: [
+        {
+          id: 1,
+          name: "Alfredo Rojas",
+          url: require("@/assets/img/alfredo-rojas.jpg"),
+        },
+        {
+          id: 2,
+          name: "Mariana Camacho",
+          url: require("@/assets/img/mariana-camacho.jpg"),
+        },
+        {
+          id: 3,
+          name: "Jesus Chamin Castro",
+          url: require("@/assets/img/jesus-chamin-castro.jpg"),
+        },
+        {
+          id: 4,
+          name: "Neomar Cepeda",
+          url: require("@/assets/img/neomar-cepeda.jpg"),
+        },
+        {
+          id: 5,
+          name: "Edgar Petit",
+          url: require("@/assets/img/edgar-petit.jpg"),
+        },
+      ],
     };
+  },
+  methods: {
+    play() {
+      // play on mount
+    },
   },
 };
 </script>
 <template>
   <Layout>
-    <section id="" class="bg-black overflow-hidden relative h-auto">
-      <div class="flex justify-center py-8 my-2">
-        <img
-          class="w-[350px] lg:w-full"
-          src="../../assets/img/banner-main.jpg"
-          alt="ar-stereo"
-        />
+    <section id="carousel" class="relative h-auto overflow-hidden bg-black">
+      <div>
+        <Carousel class="h-[800px]" :images="images" @play="play" />
       </div>
     </section>
 
-    <section id="home" class="h-screen bg-black overflow-hidden relative">
+    <section id="home" class="relative h-screen overflow-hidden bg-black">
       <div class="flex justify-center py-8 my-2 h-100">
         <figure class="flex-col flex w-[350px] lg:w-[600px]">
           <img
@@ -43,6 +73,7 @@ export default {
           />
           <!-- Autoplay is allowed. -->
           <iframe
+            id="player"
             src="https://sonic.cloudstreaming.eu/cp/widgets/player/single/?p=8036"
             height="110"
             width="100%"
@@ -53,9 +84,9 @@ export default {
       </div>
     </section>
 
-    <section id="" class="bg-black overflow-hidden relative">
-      <div class="bg-black-light flex justify-center text-white">
-        <div class="py-2 flex items-center mx-2">
+    <section id="" class="relative overflow-hidden bg-black">
+      <div class="flex justify-center text-white bg-black-light">
+        <div class="flex items-center py-2 mx-2">
           <img
             class="w-[100px] lg:w-[250px]"
             :src="alfredoRojas"
@@ -63,7 +94,7 @@ export default {
             height="250"
           />
         </div>
-        <div class="py-2 flex items-center mx-2">
+        <div class="flex items-center py-2 mx-2">
           <img
             class="w-[100px] lg:w-[250px]"
             :src="tv"
@@ -71,7 +102,7 @@ export default {
             height="250"
           />
         </div>
-        <div class="py-2 flex items-center mx-2">
+        <div class="flex items-center py-2 mx-2">
           <img
             class="w-[100px] lg:w-[250px]"
             :src="notiAr"
